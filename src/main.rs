5b4200af4967f7d5756c84041e836a19dd51e840
Folkert <folkert@folkertdev.nl>
2021-11-26 15:07:47 +0100

    finish implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+pub fn crypto_hash_sha512_tweet(data: &[u8]) -> [u8; 64] {
+    use std::mem::MaybeUninit;
+
+    let mut result = MaybeUninit::uninit();
+
+    unsafe {
+        tweetnacl_bindgen::bindings::crypto_hash_sha512_tweet(
+            result.as_mut_ptr() as *mut _,
+            data.as_ptr(),
+            data.len() as _,
+        );
+
+        result.assume_init()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("output: {:?}", crypto_hash_sha512_tweet(b"foobarbaz"));
 }
