6a0011b6f04c0a303487176568237f1b01cbb2b1
Folkert <folkert@folkertdev.nl>
2021-11-26 15:04:24 +0100

    silence warnings

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    cc::Build::new().file("tweetnacl.c").compile("tweetnacl"); // outputs `libtweetnacl.a`
+    cc::Build::new()
+        .warnings(false)
+        .extra_warnings(false)
+        .file("tweetnacl.c")
+        .compile("tweetnacl"); // outputs `libtweetnacl.a`
 }
